622f22bb2ea53d39d3e14d49c41e85fdd7b34d27
Guo <wguoaa@connect.ust.hk>
2018-05-02 13:14:14 +0800

    result

--- DPS.m
+++ DPS.m
@@ -4,7 +4,7 @@ addpath('lib/gco/matlab','lib/S2_Sampling_Suite/S2_Sampling_Toolbox',...
     'lib/nearestneighbour','lib/surfPeterKovesi');
 
 %GCO_UnitTest; % Get GCO lib initialized
-path = 'data/data02';
+path = 'data/data03';
 type = '*.bmp';
 
 % Resampling
@@ -21,4 +21,4 @@ type = '*.bmp';
 [ refined_normals ] = MRF_refined( init_normals,gray_imgs,I);
 
 %build 3d model
-model(refined_normals,img(:,:,:,I)/255);
+model(refined_normals,imgs(:,:,:,I)/255);
